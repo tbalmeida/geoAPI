36847f8c4130f7aa37589b55a0a43149742763c0
tbalmeida <tbalmeida@gmail.com>
2020-10-06 15:16:20 -0600

    20-10-06 - morning class almost ready

---
+++ geoAPI.Models/Entities/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace geoAPI.Models.Entities
+{
+  public class Company
+  {
+    [Key]  
+    public Guid Id { get; set; }
+
+    [Required]
+    public String Name { get; set; }
+  }
+}
---
+++ geoAPI.Models/ViewModels/CompanyVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace geoAPI.Models.ViewModels
+{
+  public class CompanyVM
+  {
+    public Guid Id { get; set; }
+
+    public String Name { get; set; }
+
+  }
+}
