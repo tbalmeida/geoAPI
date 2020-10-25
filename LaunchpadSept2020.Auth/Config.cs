852f6c2ff7ab52bb50fb3110ff6790723d5cf3c7
Thiago Almeida <tbalmeida@gmail.com>
2020-10-25 12:03:11 -0600

    Added Identity Server

---
+++ LaunchpadSept2020.Auth/Config.cs
@@ -0,0 +1,52 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaunchpadSept2020.Auth
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("geoapi.scope", "geoAPI")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "mobile",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("EcBSm40SIOmHthHZZTSfWyZjAC2Y7vws".Sha256())
+                    },
+                    AllowedScopes = {"geoapi.scope"}
+                }
+            };
+
+        public static List<TestUser> Users =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+    }
+}
