ed7220d2a7e012a5dea7a3a1c0d5391a8de517d2
Thiago Almeida <tbalmeida@gmail.com>
2020-10-25 17:50:22 -0600

    Added app project

---
+++ geoAPI.App/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using geoAPI.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace geoAPI.App
+{
+  public class ApplicationDbContext: IdentityDbContext<User>
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+      {
+
+      }
+
+      public DbSet<Company> Companies { get; set; }
+  }
+}
---
+++ geoAPI.App/Repositories/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,15 @@
+﻿using geoAPI.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geoAPI.App.Repositories.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        Task<CompanyVM> Create(CompanyCreateVM src);
+
+        Task<List<CompanyVM>> GetAll();
+    }
+}
